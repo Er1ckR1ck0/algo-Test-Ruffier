1e6e612f2e65718f627f10984b7a57f17af060ed
Er1ckR1ck0 <eryckov37@gmail.com>
2025-12-13 16:55:43 +0300

    Добавление таймера и логики управления состоянием на экране второго этапа

--- frontend/components/seconds.py
+++ frontend/components/seconds.py
@@ -1,15 +1,20 @@
 from kivy.uix.label import Label
 from kivy.clock import Clock
+from kivy.properties import BooleanProperty
 
 class Seconds(Label):
+    done = BooleanProperty(False)
+    
     def __init__(self, total, **kwargs):
         super().__init__()
+        self.size_hint = (1, .25)
         self.restart(total=total)
         
     def get_text(self):
         return f"Осталось: {self.total} сек."
         
     def restart(self, total, **kwargs):
+        self.done = False
         self.total = total
         self.text = self.get_text()
 
@@ -21,4 +26,5 @@ class Seconds(Label):
         self.text = self.get_text()
         if self.total <= 0:
             self.text = "Стоп!"
+            self.done = True
             return False
--- frontend/screen/third_screen.py
+++ frontend/screen/third_screen.py
@@ -1,3 +1,4 @@
+import time
 import backend.data as data
 from backend.instructions import txt_sits
 from frontend.components import *
@@ -8,11 +9,24 @@ class SecondScreen(Screen):
         super().__init__(**kw)
         main_layout = BoxLayout(orientation="vertical")
         instruction = Label(text=txt_sits)
-
+        
+        self.timer = Seconds(4)
+        self.timer.bind(done=self.next)
+        
         self.button = MyButton(self, "third_screen", "left", text="Продолжить")
-        self.button.on_press = self.button.next
-
+        self.button.on_press = self.next
+        
         main_layout.add_widget(instruction)
+        main_layout.add_widget(self.timer)
         main_layout.add_widget(self.button)
 
         self.add_widget(main_layout)
+
+    def next(self, *args):
+        if self.timer.done:
+            self.button.disabled = False
+            self.button.next()
+        else:
+            self.button.text = "Подождите!"
+            self.button.disabled = True
+            self.timer.start()
